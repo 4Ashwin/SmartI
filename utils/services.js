77a4cff8fea61b8a3778723e12359908592b2962
SreehariRajan <77159058+SreehariRajan@users.noreply.github.com>
2023-07-30 22:20:53 +0530

    Update services.js

--- utils/services.js
+++ utils/services.js
@@ -4,7 +4,7 @@ import { ethers } from "ethers";
 
 let eth;
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDJmYjE2MGNjMDUxYTE0OUFhM2NGMkE0QWE3QjJDQWJCYWEyYjgxODQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NzAwNTE1ODAwMzgsIm5hbWUiOiJ0cGdoaCJ9.H3lIt8wLVmlkXCaipxByAVJmjOHGSJYcrB2UtbiWJ1A"
+const token = "YOUR TOKEN"
 
 const storage = new Web3Storage({ token });
 
@@ -13,7 +13,7 @@ if (typeof window !== "undefined") {
     eth = window.ethereum;
 };
 function getAccessToken() {
-    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDJmYjE2MGNjMDUxYTE0OUFhM2NGMkE0QWE3QjJDQWJCYWEyYjgxODQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NzAwNTE1ODAwMzgsIm5hbWUiOiJ0cGdoaCJ9.H3lIt8wLVmlkXCaipxByAVJmjOHGSJYcrB2UtbiWJ1A"
+    return token
 }
 
 export function makeStorageClient() {
